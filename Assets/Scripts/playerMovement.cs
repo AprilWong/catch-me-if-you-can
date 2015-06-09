706a442b03a6f5cab3718a902a9f67fd6eef36e3
AprilWong <aprilwang.zju@gmail.com>
2015-06-09 21:11:53 +0800

    add 3D draft assets, add playermovement, add cb camera

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+	
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 moveDirection = new Vector3 (h, 0, v);
+		moveDirection = transform.TransformDirection (moveDirection);
+
+		controller.Move (moveDirection * Time.deltaTime);
+
+
+	}
+}
